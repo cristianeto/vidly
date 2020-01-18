48909d3006a7d8f8ef14bbcb63c1de7acb415e24
Cristian Guamán <cristian.guaman@icloud.com>
2020-01-17 20:31:00 -0500

    Forms 19- Extracting Helper Rendering Methods

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 
-const Input = ({ name, label, value, onChange }) => {
+const Input = ({ name, label, error, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
       <input
-        value={value}
-        onChange={onChange}
         autoFocus
-        id={name}
+        {...rest}
         name={name}
-        type="text"
+        id={name}
         className="form-control"
       />
+      {/* La linea de abajo significa: Si error is truthy se rendizara el div, caso es falsy ingora el div */}
+      {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
 };
