bd083d34fb841ff66aaad006d31b83dca9424322
Cristian Guamán <cristian.guaman@icloud.com>
2020-01-17 20:46:20 -0500

    Forms 21. Code Review: 20- Register form

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import LoginForm from "./components/loginForm";
 import "./App.css";
+import RegisterForm from "./components/registerForm";
 
 class App extends Component {
   state = {};
@@ -17,6 +18,7 @@ class App extends Component {
         <NavBar />
         <main className="container">
           <Switch>
+            <Route path="/register" component={RegisterForm} />
             <Route path="/login" component={LoginForm} />
             <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={Movies} />
