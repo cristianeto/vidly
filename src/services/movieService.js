6112ce8d02f17c3bc0eacea1e9b2a1a059802262
Cristian Guamán <cristian.guaman@icloud.com>
2020-02-01 15:22:02 -0500

    Calling Backend Service 28- Populating the form

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -6,6 +6,12 @@ const apiEndpoint = apiUrl + "/movies";
 export function getMovies() {
   return http.get(apiEndpoint);
 }
+export function getMovie(movieId) {
+  return http.get(apiEndpoint + "/" + movieId);
+}
+
+export function saveMovie(movie) {}
+
 export function deleteMovie(movieId) {
   return http.delete(apiEndpoint + "/" + movieId);
 }
